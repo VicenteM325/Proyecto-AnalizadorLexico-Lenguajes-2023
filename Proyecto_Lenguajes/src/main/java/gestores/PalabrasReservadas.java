5a1f75e2c1062f0b553ee917964fdc8280da1c41
Vicente Matias <vicente.matias.161@gmail.com>
2023-08-14 03:52:20 -0600

    Adding class Identificador & PalabrasReservadas to gestores package

---
+++ Proyecto_Lenguajes/src/main/java/gestores/PalabrasReservadas.java
@@ -0,0 +1,31 @@
+package gestores;
+
+public class PalabrasReservadas {
+    String [] palabrasReservadas = {"and", "as", "assert", "break", "class", "continue", "def","del", "elif", "else", "except", "False", "finally", "for", "from", "global", "if", "import", "in", "is", "lambda", "None", "nonlocal", "not", "or", "pass", "raise", "return", "True", "try", "while", "with", "yield"};
+    String [] palabrasBooleanas = {"True", "False"};
+    String [] palabrasLogicas = {"AND", "OR", "NOT"};
+    public void getTokenReservadas(String token){
+        for (String valor : palabrasReservadas) {
+            if (valor.equals(token)) {
+                Identificador.estadoActual = 10;
+                return;
+            }
+        }
+    }
+    public void getTokenBooleano(String token){
+        for(String valor : palabrasBooleanas){
+            if(valor.equals(token)){
+                Identificador.estadoActual = 12;
+                return;
+            }
+        }
+    }
+    public void getTokenLogico(String token){
+        for (String valor : palabrasLogicas){
+            if(valor.equals(token)){
+                Identificador.estadoActual = 8;
+            }
+        }
+    }
+    
+}

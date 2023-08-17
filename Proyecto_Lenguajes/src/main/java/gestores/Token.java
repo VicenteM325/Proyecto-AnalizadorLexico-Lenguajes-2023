6f8bc5a5a47e4d281c1533b057fb8714684d0db2
Vicente Matias <vicente.matias.161@gmail.com>
2023-08-17 00:08:48 -0600

    Adding class Token for its information

---
+++ Proyecto_Lenguajes/src/main/java/gestores/Token.java
@@ -0,0 +1,34 @@
+package gestores;
+
+
+public class Token {
+    
+        private String tipo;
+    private String valor;
+    private int fila;
+    private int columna;
+
+    public Token(String tipo, String valor, int fila, int columna) {
+        this.tipo = tipo;
+        this.valor = valor;
+        this.fila = fila;
+        this.columna = columna;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getToken() {
+        return valor;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+    
+}
